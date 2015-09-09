667f39a8da6f42427c795cab4353f4113a78e2aa
Peter Blackburn <peter.blackburn@live.co.uk>
2015-09-09 22:50:25 +0100

    chapter4

--- assets/myJs/videos-page2.js
+++ assets/myJs/videos-page2.js
@@ -7,25 +7,54 @@ angular.module('brushfire_videosPage', [])
 });
 
 angular.module('brushfire_videosPage').controller('PageCtrl', [
-	'$scope', '$timeout',
-	function($scope, $timeout ){
-		$scope.videosLoading = true;
-		$timeout(function afterRetrievingVideos() {
-			var _videos = [{
-				title: 'PSY - GANGNAM STYLE (강남스타일) M/V',
-				src: 'https://www.youtube.com/embed/9bZkp7q19f0'
-			}, {
-				title: 'Justin Bieber - Baby ft. Ludacris',
-				src: 'https://www.youtube.com/embed/kffacxfA7G4'
-			}, {
-				title: 'Charlie bit my finger - again !',
-				src: 'https://www.youtube.com/embed/_OBlgSz8sSM'
-			}];
+	'$scope', '$http',
+	function($scope, $http) {
 
+		$scope.videosLoading = true;
+
+		 $scope.submitVideosError = false;
+
+		// $http.get('/video?sort=id desc')
+		// .then(function onSuccess(sailsResponse) {
+		// 	$scope.videos = sailsResponse.data;
+		// })
+		// .catch(function onError(sailsResponse) {
+		// 	if (sailsResponse.data.status == '404') {
+		// 		return;
+		// 	}
+		// 	console.log("An unexpected error occured: " + sailsResponse.data.statusText);
+		// })
+		// .finally(function eitherWay() {
+		// 	$scope.videosLoading = false;
+		// });
+
+		io.socket.get('/video?sort=id desc', function whenServerResponds(data, JWR) {
 			$scope.videosLoading = false;
 
-			$scope.videos = _videos;
-		}, 750);
+			if (JWR.statusCode >= 400) {
+				$scope.submitVideosError = true;
+				console.log('something bad happened');
+				return;
+			}
+			$scope.videos = data;
+
+			$scope.$apply();
+
+			io.socket.on('video', function whenAVideoIsCreatedUpdatedOrDestroyed(event) {
+
+        // Add the new video to the DOM
+        $scope.videos.unshift({
+          title: event.data.title,
+          src: event.data.src,
+
+        });
+
+        // Apply the changes to the DOM
+        // (we have to do this since `io.socket.get` is not a
+        // angular-specific magical promisy-thing)
+        $scope.$apply();
+      });
+		});
 
 		$scope.submitNewVideo = function() {
 
@@ -34,24 +63,57 @@ angular.module('brushfire_videosPage').controller('PageCtrl', [
 			}
 
 			var _newVideo = {
-				title: $scope.newVideoTitle,
+				title: $scope.newVideoTitle + "!!!",
 				src: $scope.newVideoSrc
 			};
 
 			var parser = document.createElement('a');
 			parser.href = _newVideo.src;
-			var youtubeID = parser.search.substring(parser.search.indexOf("=")+1, parser.search.length);
+			var youtubeID = parser.search.substring(parser.search.indexOf("=") + 1, parser.search.length);
 
 			_newVideo.src = 'https://www.youtube.com/embed/' + youtubeID;
 
 			$scope.busySubmittingVideo = true;
 
-			$timeout(function() {
+			// $http.post('/video', {
+			// 	title: _newVideo.title,
+			// 	src: _newVideo.src
+			// })
+			// .then(function onSuccess(sailsResponse) {
+			// 	$scope.videos.unshift(_newVideo);
+			// })
+			// .catch(function onError(sailsResponse) {
+			// 	console.log("An unexpected error occurred: " + sailsResponse.data.statusText);
+			// })
+			// .finally(function eitherWay() {
+			// 	$scope.busySubmittingVideo = false;
+			// 	$scope.newVideoTitle = '';
+			// 	$scope.newVideoSrc = '';
+			// });
+
+			io.socket.post('/video', {
+				title: _newVideo.title,
+				src: _newVideo.src
+			}, function whenServerResponds(data, JWR) {
+
+				$scope.videosLoading = false;
+
+				if (JWR.statusCode >= 400) {
+					$scope.submitVideosError = true;
+					console.log('something bad happened');
+					return;
+				}
+
 				$scope.videos.unshift(_newVideo);
+
 				$scope.busySubmittingVideo = false;
+
 				$scope.newVideoTitle = '';
 				$scope.newVideoSrc = '';
-			}, 750);
+
+				$scope.$apply();
+			});
+
 		};
 	}
 ]);
