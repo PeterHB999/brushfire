1853fcf811bcf9a47863fb60efbe3d1c5dd7c10f
Peter Blackburn <peter.blackburn@live.co.uk>
2015-09-12 20:00:09 +0100

    chapter5

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,7 +11,43 @@
 
 module.exports.bootstrap = function(cb) {
 
-  // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  Video.count().exec(function(err, numVideos) {
+    if (err) {
+      return cb(err);
+    }
+    if (numVideos > 0) {
+      console.log('Number of video records: ', numVideos);
+      return cb();
+    }
+    var Youtube = require('machinepack-youtube');
+
+    // List Youtube videos which match the specified search query.
+    Youtube.searchVideos({
+    query: 'grumpy cat',
+    apiKey: 'AIzaSyDGCzwI979WEWYEUbsRZK6dTEadc6nyLAE',
+    limit: 15,
+    }).exec({
+      // An unexpected error occurred.
+      error: function (err){
+        return cb(err);
+      },
+      // OK.
+      success: function (result) {
+        _.each(result, function(video) {
+          video.src = 'https://www.youtube.com/embed/' + video.id;
+          delete video.description;
+          delete video.publishedAt;
+          delete video.id;
+          delete video.url;
+        });
+        Video.create(result).exec(function(err, videoRecordsCreated) {
+          if (err) {
+            return cb(err);
+          }
+          console.log(videoRecordsCreated);
+          return cb();
+        });
+      },
+    });
+  });
 };
--- config/env/production.js
+++ config/env/production.js
@@ -34,5 +34,5 @@ module.exports = {
   // log: {
   //   level: "silent"
   // }
-
+  port: process.env.port
 };
